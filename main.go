a52bf3d9ee437a0cdfd635c92faedec75e2512f9
jailton.junior <jailton.junior@iterative.com.br>
2021-02-24 09:29:30 -0300

    Testando o cron publicado

--- main.go
+++ main.go
@@ -16,33 +16,12 @@ import (
 func main() {
 	environments.New()
 
-	fmt.Printf("SqlConnectionString: %s\n", environments.SqlConnectionString)
-
 	sqlConnection := database.NewConnection()
 	defer sqlConnection.Disconnect()
 
 	ioc.New(sqlConnection)
 
 	c := cron.New()
-
-	timer := shared.NewTime()
-
-	startDate := timer.StartDate()
-	endDate := timer.EndDate()
-
-	a := ioc.AccountRepository
-	b, err := a.AccountsByDate(startDate, endDate)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, i := range b {
-		fmt.Printf("AccountId: %s\n", i.AccountId)
-		fmt.Printf("AccountDate: %s\n", i.AccountDate)
-		fmt.Printf("Description: %s\n", i.Description)
-		fmt.Printf("Value: %f\n", i.Value)
-		fmt.Println("-------")
-	}
 
 	entryID, err := c.AddFunc(environments.Cron, handle)
 	if err != nil {
@@ -57,4 +36,24 @@ func main() {
 
 func handle() {
 	fmt.Printf("Executado em: %s\n", time.Now())
+
+	timer := shared.NewTime()
+
+	startDate := timer.StartDate()
+	endDate := timer.EndDate()
+
+	accountRepository := ioc.AccountRepository
+
+	accounts, err := accountRepository.AccountsByDate(startDate, endDate)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, a := range accounts {
+		fmt.Printf("AccountId: %s\n", a.AccountId)
+		fmt.Printf("AccountDate: %s\n", a.AccountDate)
+		fmt.Printf("Description: %s\n", a.Description)
+		fmt.Printf("Value: %f\n", a.Value)
+		fmt.Println("-------")
+	}
 }
